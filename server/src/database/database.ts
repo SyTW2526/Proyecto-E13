3c11788b7ce6d5ce12aab33d99c9d891e46dc5bc
Joelsaavedra1 <alu0101437415@ull.edu.es>
2025-10-28 19:40:59 +0000

    feature: setup database

--- server/src/database/database.ts
+++ server/src/database/database.ts
@@ -1,1 +1,7 @@
-console.log("Backend setup.");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["error"],
+});
+
+export default prisma;
---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("\nServidor iniciado correctamente");
+});
