7b4397da057e85d0244ceaf85b1bf3d170a3c8e3
Tomás Pino <tomas.pino.perez03@gmail.com>
2025-11-12 21:52:18 +0000

    Revert "Feature/replace emojis with icons & finish nologin home page"

--- client/src/pages/public/loginPage.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * @file LoginPage.tsx
- * @description Página de inicio de sesión que utiliza Redux para autenticación.
- */
-import { LoginForm } from "@/components/loginForm";
-
-export default function LoginPage() {
-  return <LoginForm forceMode="login" linkTo="/register" />;
-}
--- client/src/types/team.ts
+++
@@ -1,7 +0,0 @@
-export interface TeamMember {
-  name: string;
-  email: string;
-  url: string;
-  avatarUrl: string;
-  ringClass: string;
-}
